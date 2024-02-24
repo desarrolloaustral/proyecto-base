2dad34751c7fdb8ecbea45f12c804f21d6194438
Pablo Romano <patoromano22@gmail.com>
2024-02-23 21:09:08 -0300

    correccion links iconos clientes

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,7 +3,7 @@ const services = [
         title: "BrokStudio",
         description: "Portal de noticias",
         icon: `<svg width="24" height="24">
-        <image href="https://brokstudio.com/assets/img/logos/isotipo.svg" width="24" height="24" />
+        <image href="https://www.desarrolloaustral.com/clients-logos/brokstudio.svg" width="24" height="24" />
       </svg>`,
         href: 'https://brokstudio.com/',
     },
@@ -19,7 +19,7 @@ const services = [
         title: "PicPlug",
         description: "Portal de noticias urbanas",
         icon: `<svg width="24" height="24">
-        <image href="https://www.picplug420.com/assets/img/logo.png" width="24" height="24" />
+        <image href="https://www.desarrolloaustral.com/clients-logos/picplug.png" width="24" height="24" />
       </svg>`,
         href: 'https://www.picplug420.com/',
     },
@@ -27,7 +27,7 @@ const services = [
         title: "Dolores Morón",
         description: "Artista Plástica",
         icon: `<svg width="24" height="24">
-        <image href="https://doloresmoron.com.ar/assets/img/title-img.jpg" width="24" height="24" />
+        <image href="https://www.desarrolloaustral.com/clients-logos/doloresmoron.jpg" width="24" height="24" />
       </svg>`,
         href: 'https://doloresmoron.com.ar/',
     },
