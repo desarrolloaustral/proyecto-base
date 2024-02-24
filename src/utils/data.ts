49b4d135239811b93855ac28d5cc4488c1d1b88b
Pablo Romano <patoromano22@gmail.com>
2024-02-23 21:05:21 -0300

    target blank e iconos clientes

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,30 +2,34 @@ const services = [
     {
         title: "BrokStudio",
         description: "Portal de noticias",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-        href:'https://brokstudio.com/',
+        icon: `<svg width="24" height="24">
+        <image href="https://brokstudio.com/assets/img/logos/isotipo.svg" width="24" height="24" />
+      </svg>`,
+        href: 'https://brokstudio.com/',
     },
     {
         title: "Desde la estepa",
         description: "Diseño de identidad de imagen",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-        href:'',
+        icon: `<svg width="24" height="24">
+        <image href="https://www.desarrolloaustral.com/clients-logos/desdelaestepa.svg" width="24" height="24" />
+      </svg>`,
+        href: '',
     },
     {
         title: "PicPlug",
         description: "Portal de noticias urbanas",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-        href:'https://www.picplug420.com/',
+        icon: `<svg width="24" height="24">
+        <image href="https://www.picplug420.com/assets/img/logo.png" width="24" height="24" />
+      </svg>`,
+        href: 'https://www.picplug420.com/',
     },
     {
         title: "Dolores Morón",
         description: "Artista Plástica",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-        href:'https://doloresmoron.com.ar/',
+        icon: `<svg width="24" height="24">
+        <image href="https://doloresmoron.com.ar/assets/img/title-img.jpg" width="24" height="24" />
+      </svg>`,
+        href: 'https://doloresmoron.com.ar/',
     },
 ];
 
