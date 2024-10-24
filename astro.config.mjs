d6d70d5184bc63d2cce267577f3ec3c31736c735
Pablo Romano <patoromano22@gmail.com>
2024-10-24 17:07:49 -0300

    Cambios para Indexacion SEO
    [+] Se instalo sitemap para astro.
    [+] Se crearon los archivos robots y sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://desarrolloaustral.com',
+  integrations: [tailwind(), sitemap()]
 });
