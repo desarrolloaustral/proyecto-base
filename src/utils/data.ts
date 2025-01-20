6032232208a84f30aee3149440c1db5d10a7f53a
Pablo Romano <patoromano22@gmail.com>
2025-01-20 18:56:53 -0300

    Fechas y datos dinamicos, cambios en clientes
    Se modificaron los numero de clientes, años de experiencia de forma dinamica.
    
    El componente integrantes del equipo se modificó para que se renderize dinámicamente.
    
    El año del footer se muestra de forma dinámica.
    
    Se modificaron datos de clientes.

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,4 @@
-const services = [
+const clients = [
     {
         title: "BrokStudio",
         description: "Portal de noticias",
@@ -11,13 +11,13 @@ const services = [
     },
     {
         title: "Desde la estepa",
-        description: "Diseño de identidad de imagen",
+        description: "Sitio web de estética y belleza",
         icon: `<svg width="24" height="24">
         <image href="https://www.desarrolloaustral.com/clients-logos/desdelaestepa.svg" width="24" height="24" />
       </svg>`,
-        href: '',
-        bg: `background-image: url('https://www.desarrolloaustral.com/clients-logos/desdelaestepa.svg')`,
-        target: '_top',
+        href: 'https://desdelaestepa.com.ar',
+        bg: `background-image: url('https://www.desarrolloaustral.com/clients-logos/desdelaestepa.jpg')`,
+        target: '_blank',
     },
     {
         title: "PicPlug",
@@ -41,7 +41,7 @@ const services = [
     },
     {
       title: "Delta Workout",
-      description: "Diseño de identidad de imagen",
+      description: "Gym - Diseño",
       icon: `<svg width="24" height="24">
       <image href="https://www.desarrolloaustral.com/clients-logos/deltaworkout.svg" width="24" height="24" />
     </svg>`,
@@ -61,4 +61,38 @@ const services = [
     },
 ];
 
-export { services };
+const members = [
+  {
+      name: "Denis Rybier",
+      src: "/images/denis.webp",
+      href: `https://www.linkedin.com/in/denis-luis-rybier-4a8237229/`,
+      target: "_blank",
+  },
+  {
+    name: "Ain García Nahuelanca",
+    src: "/images/ain.webp",
+    href: `https://www.linkedin.com/in/ainlautaro/`,
+    target: "_blank",
+  },
+  {
+    name: "Sebastián Cereminati",
+    src: "/images/sebastian.webp",
+    href: `https://www.linkedin.com/in/sebastian-cereminati-paez-ab8681209/`,
+    target: "_blank",
+  },
+  {
+    name: "Pablo Romano",
+    src: "/images/pablo.webp",
+    href: `https://www.linkedin.com/in/pablo-jesus-romano/`,
+    target: "_blank",
+  },
+];
+
+const currentDate = new Date();
+const initDate = new Date(2021, 8 - 1, 6); 
+const msDiff = currentDate.getTime() - initDate.getTime();
+const msByYear = 1000 * 60 * 60 * 24 * 365.25;
+const years = Math.floor(msDiff / msByYear);
+const yearsOfExperience = years < 3 ? 3 : years;
+
+export { clients, members, yearsOfExperience };
